aea751390bfcbdf0715f69f35b8b98c9baa2f00e
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-07 11:49:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speech)
-  if speech == "#{speech}"downcase
+  if speech == "#{speech}".downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif speech == "#{speech}".upcase
     return "NO, NOT SINCE 1938!"
